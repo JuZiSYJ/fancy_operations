60510d4f1c82026172f709ec1aeb0216eb97d38f
JuZiSYJ <38501523+JuZiSYJ@users.noreply.github.com>
2021-08-06 10:30:57 +0800

    Update conv_utils.py
    add  merge  1×1+3×3 -> 3x3

--- conv_utils.py
+++ conv_utils.py
@@ -268,6 +268,58 @@ class ReDCT(nn.Module):
             rearrange_weigth[k+1, ...] = src_weight[index, ...]
         return rearrange_weigth
 
+    def conv_merge():
+    '''
+    1*1  +  3*3  ->  3*3,  no bias
+    :return:
+    '''
+    x = torch.rand(5,3,112,112)
+    conv_1 = nn.Conv2d(3,2,1,1,0, bias= False) # c d 1 1
+    conv_3 = nn.Conv2d(2,4,3,1,1, bias= False) # d e 3 3
+
+    new_conv_1 = nn.Conv2d(2,3,1,1, bias=False)
+    new_conv_1.weight.data = conv_1.weight.data.permute(1,0,2,3) # d c 1 1
+
+    conv_merge = nn.Conv2d(3,4,3,1,1,bias=False)
+    merge_weight = new_conv_1(conv_3.weight) # d c 3 3
+    conv_merge.weight.data = merge_weight
+
+    y1 = conv_3(conv_1(x))
+    y2 = conv_merge(x)
+    print(torch.sum(torch.abs(y1 - y2))) # 0.0036
+
+def conv_merge2():
+    '''
+    3*3 + 1*1  -> 3*3
+    :return: 
+    '''
+    ori_3_3 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3, stride = 1, padding = 1, bias=True)
+    ori_1_1 = nn.Conv2d(in_channels=10, out_channels=6, kernel_size= 1, stride=1, padding=0, bias=True)
+
+    weight_3_3_ = ori_3_3.weight.data.clone()
+    weight_1_1_ = ori_1_1.weight.data.clone()
+
+    bias_3_3_ = ori_3_3.bias.data.clone()
+    bias_1_1 = ori_1_1.bias.data.clone()
+
+
+    reweight_3_3 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=1, bias=True)
+    print(reweight_3_3.weight.data.shape)
+
+    for i in range(weight_1_1_.shape[0]):
+        reweight_3_3.weight.data[i,...] = torch.sum(weight_3_3_ * weight_1_1_[i,...].unsqueeze(1), dim=0)
+
+        reweight_3_3.bias.data[i] = bias_1_1[i] + \
+                                    torch.sum(bias_3_3_ * weight_1_1_[i,...].squeeze(1).squeeze(1))
+
+    model = nn.Sequential(ori_3_3, ori_1_1)
+
+    x = torch.tensor(np.array(Image.open('1.jpeg'))).unsqueeze(0).permute(0,3,1,2).float()
+
+    out = model(x)
+    out2 = reweight_3_3(x)
+
+    print(torch.sum(torch.abs(out - out2)), out, out2) # tensor(5.4640, grad_fn=<SumBackward0>)
 
 
 if __name__ == '__main__':
